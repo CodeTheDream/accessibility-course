33013dbfe1cec4f74283ad8015c84ae6482c6e60
Tom Rau <tomrau@gmail.com>
2023-06-12 13:07:35 -0400

    checkin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://codethedream.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/accessibility-mini',
+  baseUrl: '/accessibility-course/',
 
 
   // GitHub pages deployment config.
